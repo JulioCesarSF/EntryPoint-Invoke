60ce53a675f76652b52060f5c4a8d2832e44a744
juliocesarsf <jcschincariolfh@gmail.com>
2018-04-22 12:46:59 -0300

    --add Project

---
+++ Carregar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Carregar
+{
+    //programa exemplo para ser carregador pelo ExecuteExe
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Task.Factory.StartNew(() => Tarefa());
+
+            Console.ReadLine();
+        }
+        private static void Tarefa()
+        {
+            int i = 0;
+            while (true)
+            {
+                i++;
+                Console.Write($"\r{i}%");
+            }
+        }
+    }
+
+}
